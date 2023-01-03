394a2ecf9ebe1eff9be4ca83801366298676f8e7
Manny <mannyjalvarez729@gmail.com>
2023-01-03 17:02:43 -0500

    gifsgit add .

--- components/PlayerSelector/index.tsx
+++ components/PlayerSelector/index.tsx
@@ -1,43 +1,63 @@
 import React from 'react';
 import * as WebBrowser from 'expo-web-browser';
-import { StyleSheet, TouchableOpacity, Pressable } from 'react-native';
-
+import { StyleSheet, ImageBackground, Pressable, Image } from 'react-native';
+import PlayerOneGif from '../../assets/gifs/Player_One_Idle.gif'
 import Colors from '../../constants/Colors';
 import { Text, View } from '../Themed';
+
+const PlayerOneGifUri = Image.resolveAssetSource(PlayerOneGif).uri
+
 
 export default function PlayerSelector() {
     let [sprite, setSprite] = React.useState(0);
 
     function handleHelpPress() {
         setSprite(sprite++)
-    
+
         // WebBrowser.openBrowserAsync(
         //     'https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet'
         // );
     }
 
     return (
-        <>
-            <View style={styles.playerSpriteCanvas}>
-                <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-                    Current Sprite is {sprite}
+        <View style={styles.playerSelectorContainer}>
+            <ImageBackground
+                source={{ uri: PlayerOneGifUri }}
+                resizeMode="cover"
+                style={styles.image}
+            >
+                <Text
+                    style={styles.helpLinkText}
+                    lightColor={Colors.light.tint}
+                >
+                    Current Sprite is: Deez Nuts!
                 </Text>
-            </View>
 
-            <View style={styles.selector}>
-                <Pressable onPress={handleHelpPress} style={styles.helpLink}>
+
+
+
+                <View style={styles.selector}>
+                <Pressable onPress={handleHelpPress} 
+                style={styles.helpLink}>
                     <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
                         {'< '}
                     </Text>
                 </Pressable>
 
-                <Pressable onPress={handleHelpPress} style={styles.helpLink}>
+                <Pressable onPress={handleHelpPress} 
+                style={styles.helpLink}>
                     <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
                         {' >'}
                     </Text>
                 </Pressable>
             </View>
-        </>
+
+
+
+
+            </ImageBackground>
+
+        </View>
     );
 }
 
@@ -46,42 +66,51 @@ export default function PlayerSelector() {
 const styles = StyleSheet.create({
     getStartedContainer: {
         alignItems: 'center',
-        marginHorizontal: 50,
+        // marginHorizontal: 50,
     },
     homeScreenFilename: {
-        marginVertical: 7,
+        // marginVertical: 7,
     },
     codeHighlightContainer: {
         borderRadius: 3,
-        paddingHorizontal: 4,
+        // paddingHorizontal: 4,
     },
     getStartedText: {
-        fontSize: 17,
-        lineHeight: 24,
+        //fontSize: 17,
+        // lineHeight: 24,
         textAlign: 'center',
     },
     helpContainer: {
-        marginTop: 15,
-        marginHorizontal: 20,
+        // marginTop: 15,
+        // marginHorizontal: 20,
         alignItems: 'center',
     },
     helpLink: {
-        paddingVertical: 15,
+        // paddingVertical: 15,
     },
     helpLinkText: {
         textAlign: 'center',
+        // paddingTop: 100,
         padding: 10,
         margin: 10,
         backgroundColor: 'green'
     },
     playerSpriteCanvas: {
-        backgroundColor: 'violet',
-        height: '50%',
-        width: '50%',
+        // height: '100%',
+        // backgroundColor: "red",
+        // alignItems: 'center',
+        // justifyContent: 'center'
     },
     selector: {
         flexDirection: 'row',
         justifyContent: 'center',
-        paddingHorizontal: 5
+        margin: '25%'
+    },
+    image: {
+        // height: '70%'
+    },
+    playerSelectorContainer: {
+        margin: 0,
+        padding: 0
     }
 });
